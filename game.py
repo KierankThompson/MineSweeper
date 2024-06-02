d98650edc8a00113d0529812cf257dce74402987
KierankThompson <thompson.kierank@gmail.com>
2024-06-02 09:31:03 -0400

    added tile

--- game.py
+++ game.py
@@ -1,13 +1,14 @@
 import pygame
 import os
+from mineNN import MineSweeperModel as MSM
 
 class Game():
 
     def __init__(self,boardSize,screenSize):
-        pygame.init()
         self.screenSize = screenSize
         self.boardSize = boardSize
         self.imageSize = screenSize[0] // boardSize[0], screenSize[1] // boardSize[1]
+        self.images = self.loadImages()
 
 
     def run(self):
@@ -18,14 +19,21 @@ class Game():
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-            self.createBoard()
-            pygame.display.flip()
+            
     
-    def createBoard(self):
 
+                
 
 
     def loadImages(self):
-        
+        filenames = [f for f in os.listdir("images") if f.endswith('.png')]
+        images = {}
+        for name in filenames:
+            imagename = os.path.splitext(name)[0] 
+            curImage = pygame.image.load(os.path.join("images", name))
+            curImage = pygame.transform.scale(curImage,self.imageSize)
+            images[imagename] = curImage
+        return images
+
 
     
